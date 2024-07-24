76af4980461b6472df22c09659d8addef4a70fcb
XiaoHaiMiao <144595676+xiaohaimiao@users.noreply.github.com>
2024-07-24 18:52:46 +0800

    V1.1 更改备份目录和备份文件名

--- 编程应用练习/壁纸备份.py
+++ 编程应用练习/壁纸备份.py
@@ -11,9 +11,19 @@
 # 2. 找到指定文件夹的地址
 # 3. 将图片备份到指定文件夹并提示“已备份”
 # 4. 用文件资源管理器打开备份文件夹
+
+# V1.1 新增功能
+# 1. 更改备份目录和备份文件名
+#   1.1 备份的图片文件名改为：WallPaper_1920x1080_20240724_572,201.jpg（572,201 是文件的存储大小）
+#   1.2 备份目录增加了 2024年07月 的子目录
+#V1.2 新增功能
+# 1. 备份桌面背景图片：%userprofile%\AppData\Roaming\Microsoft\Windows\Themes\CachedFiles
+
 import os
 import winreg
 import shutil
+from PIL import Image as Image
+import datetime as dt
 
 # 取出Windows聚焦的图片的地址
 def get_windows11_spotlight_folder():
@@ -52,16 +62,46 @@ def copy_files(src_folder_path, dst_folder_path):
     dst_dir = os.path.dirname(dst_folder_path)
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
-
+    
+    # 获取今日年月，如：2024年8月
+    year_month_str = dt.datetime.now().strftime('%Y年%m月')
+    dst_folder_path = os.path.join(dst_folder_path, year_month_str)
+    if not os.path.exists(dst_folder_path):
+        os.makedirs(dst_folder_path)
     # 遍历，并复制文件到目标文件夹
     for filename in os.listdir(src_folder_path):
         src_file_path = os.path.join(src_folder_path, filename)
-        dst_file_path = os.path.join(dst_folder_path, filename) + ".jpg"
+        dst_new_filename = construct_image_filename(src_file_path)
+        dst_file_path = os.path.join(dst_folder_path, dst_new_filename)
         print("src_file_path", src_file_path)
         print("dst_file_path", dst_file_path)
         shutil.copy2(src_file_path, dst_file_path)
 
     return True
+
+# 函数：构造图片文件的名字，并返回
+# 基于指定图片文件的信息构造，规则为 WallPaper_1920x1080_20240724_572,201.jpg
+# 其中，“1920x1080”是图片分辨率，“20240724”是图片创建日期，“572,201” 是文件的存储大小
+def construct_image_filename(image_path):
+    # 获取文件大小
+    file_size = os.path.getsize(image_path)
+    file_size_str = f"{file_size // 1024},{file_size % 1024}"
+
+    # 获取文件的创建日期，构造新文件名中的日期部分
+    file_creation_time = os.path.getctime(image_path)  # 获取文件创建时间
+    file_create_time_str = dt.datetime.fromtimestamp(file_creation_time).strftime('%Y%m%d')  # 将时间戳转换为日期格式
+    file_ext = ".jpg"
+    
+    # 获取图片的长、宽，用于构造文件名中的分辨率部分
+    img = Image.open(image_path)
+    img_width, img_height = img.size
+    img_resolution = f"{img_width}x{img_height}"
+
+    # 构造新的文件名
+    new_file_name = f"WallPaper_{img_resolution}_{file_create_time_str}_{file_size_str}{file_ext}"
+
+    # 返回新的文件名
+    return new_file_name
 
 # 打开“我的图片_壁纸备份_”
 def open_folder(folder_path):
@@ -75,6 +115,7 @@ def main():
     source_folder = get_windows11_spotlight_folder()
     dest_folder = os.path.join(get_mypictures_folder(), "_备份壁纸_")
     copy_files(source_folder, dest_folder)
+    
     open_folder(dest_folder)
     return
 
